8be0bb1da4033cb883cfa85a90ac4feca6e48145
Chris Barty <christopher.barty@my.jcu.edu.au>
2020-04-05 21:28:06 +1000

    Update subject_reader program with 'things to do' completed

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,6 +1,6 @@
 """
-CP1404/CP5632 Practical
-Data file -> lists program
+CP1404 - Week 4 Practical
+Program to display data from a list import in different ways
 """
 
 FILENAME = "subject_data.txt"
@@ -9,11 +9,14 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    print("----------")
+    print_detailed_summary(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    list_data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -21,9 +24,24 @@ def get_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+
         print(parts)  # See if that worked
         print("----------")
+        list_data.append(parts)
     input_file.close()
+    return list_data
+
+
+def print_detailed_summary(data):
+    """Print detailed summary based on list data given and format output spacing"""
+    longest_name = 0
+    # Determine longest name for width formatting
+    for item in data:
+        if len(item[1]) > longest_name:
+            longest_name = len(item[1])
+    # Print formatted data
+    for item in data:
+        print("{} is taught by {:{width}} and has {:>3} students".format(item[0], item[1], item[2], width=longest_name))
 
 
 main()
