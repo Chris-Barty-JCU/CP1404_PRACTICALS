9ea27b855d64d9145e6127d3724400222148ffe9
Chris Barty <christopher.barty@my.jcu.edu.au>
2020-04-05 15:38:01 +1000

    Further tidying up of function use.

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -3,21 +3,22 @@ CP1404 Password Checker - Week 3 Practical
 Program to get a users password and print it as asterisks
 """
 
-MIN_LENGTH = 8
+MIN_LENGTH = 10
 
 
 def main():
-    get_password()
+    user_password = get_password()
+    while not validate_password(user_password):
+        print("Invalid Password - Does Not Meet Requirements")
+        user_password = get_password()
+    print_password(user_password)
 
 
 def get_password():
     """Ask user for a password and receive input as a string"""
     print("Please enter a valid password ({} Characters or longer)".format(MIN_LENGTH))
     password = str(input("> "))
-    while not validate_password(password):
-        print("Invalid Password - Does Not Meet Requirements")
-        password = input("> ")
-    print_password(password)
+    return password
 
 
 def print_password(password):
@@ -28,10 +29,10 @@ def print_password(password):
 
 def validate_password(password):
     """Validate password based on user input"""
-    if len(password) < MIN_LENGTH:
-        return False
-    else:
+    if len(password) >= MIN_LENGTH:
         return True
+    else:
+        return False
 
 
 main()
