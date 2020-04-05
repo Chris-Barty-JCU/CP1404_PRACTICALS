4b301a1330c59f518b32a548ca1bb3ea8816dc5a
Chris Barty <christopher.barty@my.jcu.edu.au>
2020-04-05 15:03:08 +1000

    Refactor program to use functions in a more effective manner.

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,23 +1,33 @@
 """
 CP1404 Password Checker - Week 3 Practical
+Program to get a users password and print it as asterisks
 """
 
 MIN_LENGTH = 8
 
 
 def main():
-    """Program to get a users password and print it as asterisks"""
+    get_password()
+
+
+def get_password():
+    """Ask user for a password and receive input as a string"""
     print("Please enter a valid password ({} Characters or longer)".format(MIN_LENGTH))
     password = str(input("> "))
-    while not password_validation(password):
+    while not validate_password(password):
         print("Invalid Password - Does Not Meet Requirements")
         password = input("> ")
-    password_display = "*" * len(password)
-    print("Password Set ({} Characters): {}".format(len(password), password_display))
+    print_password(password)
 
 
-def password_validation(password):
-    """Determine if the provided password is valid"""
+def print_password(password):
+    """Print password in a particular format"""
+    censored_password = "*" * len(password)
+    print("Password Set ({} Characters): {}".format(len(password), censored_password))
+
+
+def validate_password(password):
+    """Validate password based on user input"""
     if len(password) < MIN_LENGTH:
         return False
     else:
